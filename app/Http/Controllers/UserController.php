950c43739d48d0a665ec847e4cbb5f19ff3aa463
taixing <1509239073@qq.com>
2018-04-08 16:57:02 +0800

    model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Flight;
 use DB;
 use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Crypt;
@@ -31,20 +32,75 @@ class UserController extends Controller
         // $a =  $this->hasher->check('123456','$2y$10$ykAdXd8ZETSL3jFqYI/wRO5vJ3J7AJe28kUQzKTKz9DEeZ3c86zsu');
         // dump($a);
         // dump($this->Crypt);
-     //    $encrypted = Crypt::encrypt('Hello world.');
+         //    $encrypted = Crypt::encrypt('Hello world.');
 
-     //    $decrypted = Crypt::decrypt($encrypted);
-     //    dump($encrypted);
-     //    dump($decrypted);
-        $users = DB::select('select * from users where id > ?', [1]);
-        // $results = DB::select('select * from users where id = :id', ['id' => 1]);
-        foreach ($users as $key => $value) {
-            dump($value);
-        }
+         //    $decrypted = Crypt::decrypt($encrypted);
+         //    dump($encrypted);
+         //    dump($decrypted);
+        // $users = DB::select('select * from users where id > ?', [1]);
+        // // $results = DB::select('select * from users where id = :id', ['id' => 1]);
+        // foreach ($users as $key => $value) {
+        //     dump($value);
+        // }
         // dump($users);
-        // return view('user.profile', ['user' =>$users]);
+        // $users = DB::table('users')->get();
+        // 解析成对象
+        // foreach ($users as $user) {
+        //   echo $user->name;
+        // }
+        // $user = DB::table('users')->where('name', 'allen.tai1')->first();
+        // $user = DB::table('users')->where('name', 'allen.tai')->value('email');
+        // echo $user;
+        // DB::table('users')->chunk(10, function($users) {
+        //         foreach ($users as $user) {
+        //             dump($user->name);
+        //             if($user->name =='allen.tai')
+        //                 return false;
+        //         }
+        // });
+        //count,sum,avg,max,min
+        //get 对应 select first 对应 find
+        // $users =  DB::table('users')->select(DB::raw('group_concat(id)'))->first();
+        // // DB::raw 原生表达式
+        // $users = DB::table('users')
+        //             ->where('id', '>', 2)
+        //             ->orWhere('name', 'Allen.tai')
+        //             ->get();
+        // DB::table('users')
+        //     ->where('name', '=', 'John')
+        //     ->orWhere(function ($query) {
+        //         $query->where('votes', '>', 100)
+        //               ->where('title', '<>', 'Admin');
+        //     })
+        //     ->get();
+        //可以构建一个匿名函数进行修改
+        //orderBy 
+        //groupBy / having / havingRaw
+        // $users =DB::table('users')->skip(5)->take(10)->get();
+        //insert()
+        //栏位 column array
+        //insertGetId 获取自增id
+        // $map['name'] = 'test';
+        // DB::table('users')->where('id',1)->update($map);
+        // 增加 减少 increment decrement
+        // 如果你希望清除整张表，也就是删除所有列并将自增ID置为0，可以使用truncate方法：
+        // DB::table('users')->truncate();
+        // sharedLock方法从而在运行语句时带一把”共享锁“。共享锁可以避免被选择的行被修改直到事务提交：
+        // lockForUpdate 锁避免选择行被其它共享锁修改或删除
+        // $users = DB::table('users')->where('id', '>', 10)->lockForUpdate()
+        // ->get();
+        $flights = Flight::findOrFail(1);
 
+
+        
+ 
+        // dump($flights->name);
+
+        
+        
     	// dump(User::findOrFail($id));
         // return view('user.profile', ['user' =>User::findOrFail($id)]);
+        // $users=DB::table('users')->paginate(10);
+        // return view('user.profile',['users'=>$users]);
     }
 }
---
+++ database/migrations/2018_04_02_060436_create_flights_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('flights', function ($table) {
+           $table->dropColumn('airline');
+
+           $table->string('name', 50)->nullable()->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
